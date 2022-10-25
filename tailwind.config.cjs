0bfe71670ebb5cf44350a87bfbad0ba5731b1907
周小琪 <37129846+zxqzy@users.noreply.github.com>
2022-10-25 15:26:29 +0800

    feat: define tailwindcss spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * numberSpacings = {
+ *   0: '0px',
+ *   1: '1px',
+ *   ...
+ *   200: '200px',
+ * }
+ */
+const numberSpacings = Array.from({ length: 201 }).reduce((acc, _, index) => {
+  acc[`${index}`] = `${index}px`
+  return acc
+}, {})
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        ...numberSpacings,
+        auto: 'auto',
+      },
+    },
   },
   plugins: [],
 }
