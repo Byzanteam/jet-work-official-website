0ea7ad75fd5edd0bdaa0fc7af839f10391d30c2c
moonRider <solar.pom@gmail.com>
2022-10-27 18:01:21 +0800

    feat: add responsive feature section

---
+++ src/components/home/features/_saas.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col md:flex-row-reverse gap-6 items-stretch">
+    <FeatureCard image="saas_feature.svg" />
+    <div class="grow-[5] basis-0 flex flex-col justify-center relative">
+      <FeatureDescription
+        :title="$t('page.home.features.saas.title')"
+        :summary="$t('page.home.features.saas.summary')"
+        :description="$t('page.home.features.saas.description')"
+      />
+      <div
+        class="absolute top-full transform -translate-y-1/4 right-0 md:w-14 w-10"
+      >
+        <img src="/images/saas_arrow.svg" class="w-full" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SaasFeature',
+}
+</script>
+
+<script setup>
+import FeatureCard from './_feature_card.vue'
+import FeatureDescription from './_feature_description.vue'
+</script>
