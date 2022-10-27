90e3130bee1a7feb28f5b9cc6e64cb9228770248
周小琪 <37129846+zxqzy@users.noreply.github.com>
2022-10-27 21:20:10 +0800

    fix: update i18n

--- src/components/home/slogan.vue
+++ src/components/home/slogan.vue
@@ -8,14 +8,14 @@
         class="md:basis-[560px] md:grow-[7] md:shrink text-white bg-gradient-aperture bg-contain bg-no-repeat"
       >
         <h1 class="text-brand-bule mt-[89px]">{{ $t('page.home.build') }}</h1>
-        <h1>{{ $t('page.home.as_you_width') }}</h1>
+        <h1>{{ $t('page.home.as_you_wish') }}</h1>
         <p class="mt-4 mb-14 text-2xl leading-[34px] text-gray-65">
           {{ $t('page.home.build.description') }}
         </p>
         <button
           class="w-[180px] h-14 border-brand-bule border-solid border rounded-lg hover:bg-brand-bule"
         >
-          {{ $t('page.home.contact') }}
+          {{ $t('page.home.contact_us') }}
         </button>
       </div>
       <!-- image container -->
