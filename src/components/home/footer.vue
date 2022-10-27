ba46f635e91af73d09b6631d0e622d53c71101c0
周小琪 <37129846+zxqzy@users.noreply.github.com>
2022-10-27 18:18:51 +0800

    fix: udpate footer

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -1,21 +1,35 @@
 <template>
   <footer class="bg-gray-08">
-    <div class="max-w-[1200px] m-auto py-14 px-5 md:px-10 xl:px-[60px]">
+    <div
+      class="max-w-[1200px] m-auto py-10 xl:py-14 px-5 md:px-10 xl:px-[60px]"
+    >
       <div
         class="flex flex-col xl:flex-row items-center justify-between py-2.5"
       >
-        <div class="flex items-center">
-          <div class="w-[60px] h-[60px]">
-            <img src="/images/logo_jet-02.png" alt="logo" />
-          </div>
-          <div class="text-gray-50 w-[186px] ml-4 leading-snug text-base">
-            {{ $t('page.home.address') }}
-          </div>
+        <div class="w-[110px]">
+          <img
+            src="/images/logo_jet.png"
+            alt="logo"
+            class="w-full object-cover"
+          />
         </div>
         <div
-          class="text-gray-50 whitespace-nowrap xl:mt-0 mt-12 text-xs font-medium"
+          class="text-gray-80 text-sm font-medium mt-[34px] xl:mt-0 text-center xl:text-left"
         >
-          {{ $t('page.home.copyright') }}
+          <div>
+            {{ $t('page.home.copyright') }}
+          </div>
+          <div class="xl:flex">
+            <div class="flex items-center mt-2">
+              <img src="/images/public_security.png" alt="public_security" />
+              <!-- TODO: use i18n -->
+              <span class="ml-2">川公安网备 51010702001785 号 </span>
+            </div>
+            <!-- TODO: use i18n -->
+            <div class="text-gray-50 xl:text-gray-80 xl:ml-2 mt-2">
+              蜀ICP备15017503号-2
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/home/header.vue
+++ src/components/home/header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="w-full sticky top-0 bg-black">
     <div class="max-w-[1200px] m-auto py-7 px-5 md:px-10 xl:px-[60px]">
-      <img src="/images/logo_jet-01.png" alt="logo_width_text" />
+      <img src="/images/logo_jet.png" alt="logo_width_text" />
     </div>
   </header>
 </template>
