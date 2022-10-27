9d893c878a8a3478b3b57314ba1a5205f3190a3b
moonRider <solar.pom@gmail.com>
2022-10-27 18:00:17 +0800

    refactor: make section title responsive

---
+++ src/components/home/_title.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="text-center flex flex-col items-center">
+    <h2
+      class="text-white md:text-[40px] md:leading-[56px] text-[32px] leading-[45px]"
+    >
+      {{ title }}
+    </h2>
+    <p
+      class="w-full md:text-base md:leading-[22px] xl:max-w-[588px] text-sm max-w-[450px] mt-2 text-gray-80"
+    >
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionTitle',
+  inheritAttrs: false,
+}
+</script>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
+</script>
