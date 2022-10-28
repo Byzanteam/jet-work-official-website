44a43266b222d041e5c33436661aa5d30fbce0a8
moonRider <solar.pom@gmail.com>
2022-10-28 19:08:53 +0800

    feat: adjust detail of footer

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -1,8 +1,8 @@
 <template>
   <footer class="bg-gray-08">
-    <div class="py-10 xl:py-14 content-container">
+    <div class="py-10 md:py-[54px] xl:py-14 content-container">
       <div
-        class="flex flex-col xl:flex-row items-center justify-between py-2.5"
+        class="flex flex-col xl:flex-row items-center justify-between py-2.5 px-6 gap-6"
       >
         <div class="w-[110px]">
           <img
@@ -12,19 +12,19 @@
           />
         </div>
         <div
-          class="text-gray-80 text-sm font-medium mt-[34px] xl:mt-0 text-center xl:text-left"
+          class="text-gray-80 text-sm font-medium mt-[34px] xl:mt-0 text-center md:text-center xl:text-left"
         >
           <div>
             {{ $t('page.home.copyright') }}
           </div>
-          <div class="xl:flex">
-            <div class="flex items-center mt-2">
+          <div class="flex md:flex-row flex-col mt-2">
+            <div class="flex items-center">
               <img src="/images/public_security.png" alt="public_security" />
               <!-- TODO: use i18n -->
               <span class="ml-2">川公安网备 51010702001785 号 </span>
             </div>
             <!-- TODO: use i18n -->
-            <div class="text-gray-50 xl:text-gray-80 xl:ml-2 mt-2">
+            <div class="text-gray-50 xl:text-gray-80 md:ml-2 md:mt-0 mt-2">
               蜀ICP备15017503号-2
             </div>
           </div>
