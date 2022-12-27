c9f46807a287ef145c32dc3c0f28655672384ccf
shaddollxz <shaddollxz@163.com>
2022-12-27 13:53:00 +0800

    feat: components-tab

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as LayoutHeader } from './layout/header.vue'
 export { default as LayoutFooter } from './layout/footer.vue'
 
 export { default as SvgIcon } from './svg-icon.vue'
+export { TabPane, TabSwitch } from './tab-switch'
