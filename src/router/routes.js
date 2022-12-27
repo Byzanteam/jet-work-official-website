8aa50e2d1a09cb6e4d67c6beae959fad4b717589
shaddollxz <shaddollxz@163.com>
2022-12-27 17:26:59 +0800

    feat: solutions-platform-form_engine

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,4 +4,9 @@ export const routes = [
     name: 'home',
     component: () => import('@/views/home/index.vue'),
   },
+  {
+    path: '/flow_engine',
+    name: 'flow_engine',
+    component: () => import('@/views/solutions/platform/flow-engine.vue'),
+  },
 ]
