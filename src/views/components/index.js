2c9f0f3028328a23a8086af02a633bd50f85f8b4
shaddollxz <shaddollxz@163.com>
2022-12-27 13:54:53 +0800

    feat: views-components-layout

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -1,1 +1,3 @@
 export { default as SolutionTitle } from './solution-title.vue'
+export { default as BlockLayout } from './block-layout.vue'
+export { default as BlockInLayout } from './block-in-layout.vue'
