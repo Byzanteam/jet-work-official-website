4d6cd12b56b8cbdaa27cb675a4656b76c009d87e
shaddollxz <shaddollxz@163.com>
2022-12-27 13:53:24 +0800

    feat: components-table

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as LayoutHeader } from './layout/header.vue'
 export { default as LayoutFooter } from './layout/footer.vue'
 
+export { default as DataTable } from './data-table.vue'
 export { default as SvgIcon } from './svg-icon.vue'
 export { TabPane, TabSwitch } from './tab-switch'
