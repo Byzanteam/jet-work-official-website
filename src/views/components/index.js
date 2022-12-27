737ecd834707b4af457a418456c833f02351db6e
shaddollxz <shaddollxz@163.com>
2022-12-27 13:55:29 +0800

    feat: views-components-selectorList

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -1,3 +1,4 @@
 export { default as SolutionTitle } from './solution-title.vue'
 export { default as BlockLayout } from './block-layout.vue'
 export { default as BlockInLayout } from './block-in-layout.vue'
+export { default as SelectorList } from './selector-list.vue'
