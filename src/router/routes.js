6d875fd17af2d65f0ba1523fb4b6864cc051a20b
shaddollxz <shaddollxz@163.com>
2022-12-28 10:02:40 +0800

    feat: solutions-scene-grassroot-index

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,4 +4,9 @@ export const routes = [
     name: 'home',
     component: () => import('@/views/home/index.vue'),
   },
+  {
+    path: '/grassroot',
+    name: 'grassroot',
+    component: () => import('@/views/solutions/scene/grassroot/index.vue'),
+  },
 ]
