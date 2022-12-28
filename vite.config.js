70ecdbb5cd955d1ee3dc8011e9ef7b4682af5348
idcjaja <838927992@qq.com>
2022-12-28 14:18:30 +0800

    chore: check pr automatically
    chore: .vue file read absolute path

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
 })
