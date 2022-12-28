4e35df5df7fca7c142b3cf78ed19f24cdac82b78
shaddollxz <56341682+shaddollxz@users.noreply.github.com>
2022-12-28 10:24:26 +0800

    feat: solutions-platform-form_engine
    平台解决方案-表单引擎 - 1

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,4 +4,9 @@ export const routes = [
     name: 'home',
     component: () => import('@/views/home/index.vue'),
   },
+  {
+    path: '/form_engine',
+    name: 'form_engine',
+    component: () => import('@/views/solutions/platform/form-engine.vue'),
+  },
 ]
