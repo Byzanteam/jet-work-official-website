e266fa3fbd3cdc15d962bc77f6936c3537baa1a3
shaddollxz <shaddollxz@163.com>
2022-12-28 14:15:31 +0800

    fix: layout fix

--- src/views/components/block-layout.vue
+++ src/views/components/block-layout.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="flex flex-col items-center content-container pb-6">
-    <div
-      class="md:px-[204px] px-[50px] md:pb-[56px] pb-[24px]"
-      :class="title ? 'pt-[72px]' : ''"
-    >
-      <h3
-        v-if="title"
-        class="text-center"
-        :class="blockDesc ? 'mb-4' : 'md:mb-[56px] mb-[24px]'"
-      >
-        {{ title }}
-      </h3>
-      <div v-if="blockDesc" class="text-gray-60 text-center">
-        {{ blockDesc }}
+  <div class="w-full">
+    <div class="flex flex-col items-center content-container pb-6">
+      <div class="md:pb-[56px] pb-[24px]" :class="title ? 'pt-[72px]' : ''">
+        <h3
+          v-if="title"
+          class="md:px-[204px] px-[50px] text-center"
+          :class="blockDesc ? 'mb-4' : 'md:mb-[56px] mb-[24px]'"
+        >
+          {{ title }}
+        </h3>
+        <div
+          v-if="blockDesc"
+          class="md:px-[204px] px-[20px] text-gray-60 text-center"
+        >
+          {{ blockDesc }}
+        </div>
       </div>
+      <slot />
     </div>
-    <slot />
   </div>
 </template>
 <script setup>
