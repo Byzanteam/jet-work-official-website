f84722566fd98aa889cb01d75cdc1363adc612e3
shaddollxz <shaddollxz@163.com>
2022-12-28 14:20:55 +0800

    fix: table style fix

--- src/views/solutions/scene/grassroot/_government-operation.vue
+++ src/views/solutions/scene/grassroot/_government-operation.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex md:flex-row flex-col gap-6">
-    <CardItem :item="valueCardItemOne" class="flex-grow-[3] bg-gray-08" />
-    <CardItem :item="valueCardItemTwo" class="flex-grow-[1] bg-gray-08" />
+    <CardItem :item="valueCardItemOne" class="flex-[3] bg-gray-08" />
+    <CardItem :item="valueCardItemTwo" class="flex-[1] bg-gray-08" />
   </div>
   <DataTable
     :headers="tableHeader"
     :body="tableItems"
-    class="my-6 overflow-x-hidden"
+    class="block w-full my-6 overflow-x-scroll"
   />
-  <BlockInLayout :title="pageText.instance" class="bg-gray-08 rounded-lg">
+  <BlockInLayout :title="pageText.instance" class="bg-gray-08 rounded-lg pb-0">
     <div class="mt-14 overflow-hidden">
       <img src="/images/grassroot/government_operation_instance.webp" />
     </div>
