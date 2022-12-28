87aa36007e287bebc1880c8bfff8606cf60bc3ef
shaddollxz <56341682+shaddollxz@users.noreply.github.com>
2022-12-28 10:24:01 +0800

    feat: solutions-components
    把通用组件提取出来为一个pr

---
+++ src/views/components/_listable-text.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul v-if="text.includes('- ')" class="list-disc list-inside">
+    <li v-for="itemValue of text.split('- ')" :key="itemValue" class="mb-4">
+      {{ itemValue }}
+    </li>
+  </ul>
+  <div v-else>
+    {{ text }}
+  </div>
+</template>
+
+<script setup>
+defineProps({ text: { type: String, required: true } })
+</script>
---
+++ src/views/components/block-in-layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="md:py-14 py-6">
+    <h4 class="md:mb-14 mb-6 text-center">
+      {{ title }}
+    </h4>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+defineProps({ title: { type: String, required: true } })
+</script>
