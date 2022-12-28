b011cecdcac3240a9b2f98a66ee678a1a379d595
shaddollxz <shaddollxz@163.com>
2022-12-28 09:50:49 +0800

    feat: solutions-scene-all_in_one_table-index

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,4 +4,10 @@ export const routes = [
     name: 'home',
     component: () => import('@/views/home/index.vue'),
   },
+  {
+    path: '/all_in_one_table',
+    name: 'all_in_one_table',
+    component: () =>
+      import('@/views/solutions/scene/all-in-one-table/index.vue'),
+  },
 ]
