6c2bd18f96195071bcf65a1de7948a1a7eaa5a1a
shaddollxz <shaddollxz@163.com>
2023-01-12 16:26:52 +0800

    fix: selector-list style

--- src/views/components/selector-list.vue
+++ src/views/components/selector-list.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="flex items-center gap-6 w-full">
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col gap-2">
       <div
         v-for="(item, index) of items"
         :key="item.title"
-        class="flex items-start p-6 gap-3 rounded-lg md:w-[384px] w-full overflow-hidden cursor-pointer"
-        :class="
-          chosedIndex === index
-            ? 'md:linear-chosed-item-bg md:text-brand-blue'
-            : ''
-        "
+        class="rounded-lg w-full md:bg-transparent bg-gray-03 dark:bg-gray-08 overflow-hidden cursor-pointer"
         @click="() => choseBlock(index)"
       >
-        <SvgIcon :path="item.icon" />
-        <div>
-          <h5 class="mb-2">
-            {{ item.title }}
-          </h5>
-          <div class="text-gray-60">{{ item.desc }}</div>
+        <div
+          class="flex items-start p-6 gap-3"
+          :class="
+            chosedIndex === index
+              ? 'md:linear-chosed-item-bg md:text-brand-blue'
+              : ''
+          "
+        >
+          <SvgIcon :path="item.icon" />
+          <div>
+            <h5 class="mb-2">
+              {{ item.title }}
+            </h5>
+            <div class="text-gray-60">{{ item.desc }}</div>
+          </div>
         </div>
       </div>
     </div>
@@ -25,10 +29,8 @@
     <div class="md:flex-1 lg:flex-[2] md:block hidden">
       <img
         :src="items[chosedIndex].img"
-        height="375"
-        width="572"
         alt=""
-        class="lg:block lg:-my-0 lg:mx-auto"
+        class="lg:block lg:-my-0 lg:mx-auto h-[375px] w-[572px] object-cover object-left"
       />
     </div>
   </div>
